90ff0507fcc84d0d16939eb3bebd1ec8ff8a3d0b
mahlad <mahlad365@gmail.com>
2012-07-23 22:40:46 +0430

    creat the pages and seprate them

---
+++ head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>ARPO</title>
+	<link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/grid.css">
+	<link rel="stylesheet" href="style.css">
+    <script src="js/html5shiv.js"></script>
+</head>
---
+++ header.php
@@ -0,0 +1,5 @@
+<?php
+	include'head.php';
+?>
+<body>
+	<div></div>
