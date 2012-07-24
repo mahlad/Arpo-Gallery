a60f206272299f3515bae98cd47cb0e779ab8222
mahlad <mahlad365@gmail.com>
2012-07-24 23:36:06 +0430

    design footer

--- footer.php
+++ footer.php
@@ -1,6 +1,33 @@
     <footer>
-    
+    	<div class="bottom">
+        	<section class="right padleft w5">
+            	<h3>پیام هفته</h3>
+                <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui
+David James on</p>
+			<div class="clear"></div>
+            </section>
+            <section class="right padleft  w5">
+            	<h3>لینک ها</h3>
+                <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui
+David James on</p>
+			<div class="clear"></div>
+            </section>
+            <section class="right  w5">
+            	<h3>دوستان</h3>
+                <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui
+David James on</p>
+			<div class="clear"></div>
+            </section>
+          <div class="clear"></div>  
+        </div>
+        <div class="copyright">
+        	<p>کلیه حقوق این سایت محفوظ و متعلق به...است</p>
+        </div>
+        <div class="clear"></div>
     </footer>
+    <div class="clear"></div>
 </div><!--container-->
+<div class="clear"></div>
+</div><!--out-->
 </body>
 </html>
