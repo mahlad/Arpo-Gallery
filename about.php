2454fd2c4b06c028d700ff44538d615d168aaaf1
mahlad <mahlad365@gmail.com>
2012-07-24 23:36:35 +0430

    correct the parents

--- about.php
+++ about.php
@@ -1,3 +1,6 @@
+<?php
+	include'head.php';
+?>
 <div class="bg-fixed">
 	<img src="images/img5.jpg">
 </div>
@@ -8,7 +11,12 @@
 	<header class="pages">
     	<h1>درباره من</h1>
     </header>
-	<div id="main"></div>
+	<div id="main">
+       	<img src="images/img_8.jpg"/>
+        <h2>طراحی زندگی من است</h2>
+        <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui, eu viverra nulla magna at massa. Proin eu iaculis ante. Cras ut augue ac arcu dignissim consequat et non odio. Cras facilisis ligula vitae ipsum sollicitudin congue. Donec volutpat consectetur rutrum. Aliquam this is highlight Mauris blandit tellus sit amet felis ullamcorper a aliquam augue posuere. Quisque tempus purus elit, eu facilisis neque.</p>
+    </div><!--main-->
+    <div class="clear"></div>
 
 <?php
 	include'footer.php';
