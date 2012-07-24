0e5a397a3de05885961f8363d287258a55f869c2
mahlad <mahlad365@gmail.com>
2012-07-24 23:37:03 +0430

    corect

--- header.php
+++ header.php
@@ -1,22 +1,20 @@
-<?php
-	include'head.php';
-?>
-<body>
+<div class="out">
 	<header class="general">
     	<article>
         	<img class="ml2" src="images/logo.png">
             <img class="ml1" src="images/logo-text.png">
             <nav class="headernav">
             	<ul>
-                	<li><a href="about.php">
-                    	<h2>بیوگرافی من</h3>
-                    	<span>آشنایی با طراح</span>
-                    </a>
-                    
+                	<li>
+                    	<a href="about.php">
+                    		<h2>بیوگرافی من</h3>
+                    		<span>آشنایی با طراح</span>
+                    	</a>
                     </li>
-                    <li><a href="">
-                    	<h2>گالری </h3>
-                    	    
+                    <li>
+                    	<a href="">
+                    		<h2>گالری </h3>
+                            <span>آرشیو طرح ها</span>
                     	</a>
                     	<ul>
                         	<li><a href=""> 1نام آلبوم </a></li>
@@ -24,9 +22,16 @@
                             <li><a href="">نام آلبوم 3</a></li>
                         </ul>
                     </li>
-                    <li><a href=""><h2>تماس با من</h3></a></li>
+                    <li>
+                    	<a href="">
+                        	<h2>تماس با من</h3>
+                            <span>ارتباط</span>
+                        </a>
+                    </li>
                    
                 </ul>
             </nav>
         </article>
+        
     </header>
+    
