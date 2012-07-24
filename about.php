33873285a5d1c989c5158d4a254cecf034ae149f
mahlad <mahlad365@gmail.com>
2012-07-24 14:57:10 +0430

    create for biography

---
+++ about.php
@@ -0,0 +1,13 @@
+<div class="bg-fixed">
+	<img src="images/img5.jpg">
+</div>
+<?php
+	include'header.php';
+?>
+<div class="container">
+	<header></header>
+	<div id="main"></div>
+
+<?php
+	include'footer.php';
+?>
