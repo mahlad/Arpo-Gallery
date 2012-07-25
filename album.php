66876e1d94f94dedfbf23174ae30848ec7e083df
mahlad <mahlad365@gmail.com>
2012-07-25 14:42:24 +0430

    design album page

---
+++ album.php
@@ -0,0 +1,42 @@
+<?php
+	include'head.php';
+?>
+<div class="bg-fixed">
+	<img src="images/img5.jpg">
+</div>
+<?php
+	include'header.php';
+?>
+<div class="container">
+	<header class="pages">
+    	<h1>آلبوم ...</h1>
+    </header>
+	<div id="main">
+    	<section class="right album padleft w8">
+        	<h3>نام طرح</h3>
+        	<img src="images/img_13.jpg" width="310">
+            <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui, eu viverra nulla magna at massa.</p>
+        </section>
+        <section class="right album w8">
+        	<h3>نام طرح</h3>
+        	<img src="images/img_6.jpg" width="310">
+            <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui, eu viverra nulla magna at massa.</p>
+        </section>
+        <div class="clear"></div>
+        <section class="right album padleft w8">
+        	<h3>نام طرح</h3>
+        	<img src="images/img_13.jpg" width="310">
+            <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui, eu viverra nulla magna at massa.</p>
+        </section>
+        <section class="right album w8">
+        	<h3>نام طرح</h3>
+        	<img src="images/img_6.jpg" width="310">
+            <p>Lorem ipsum dolor sit amet, this is highlight adipiscing elit. Quisque ultricies, risus sed semper pretium, purus dui auctor dui, eu viverra nulla magna at massa.</p>
+        </section>
+        <div class="clear"></div>
+    </div><!--main-->
+    <div class="clear"></div>
+
+<?php
+	include'footer.php';
+?>
